2834c710c7a2e95ad71d96eb56ed65714c7b8eb9
Jay Ramirez <ramirezblancasjuanm@gmail.com>
2020-07-31 00:27:19 -0500

    imagenes corregidas

--- src/app/pages/modals/edit-profile/edit-profile.page.ts
+++ src/app/pages/modals/edit-profile/edit-profile.page.ts
@@ -70,7 +70,7 @@ export class EditProfilePage implements OnInit {
 
   addBannerImage() {
     const options: CameraOptions = {
-      quality: 10,
+      quality: 5,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
@@ -120,7 +120,7 @@ export class EditProfilePage implements OnInit {
 
   addAvatarImage() {
     const options: CameraOptions = {
-      quality: 10,
+      quality: 5,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
